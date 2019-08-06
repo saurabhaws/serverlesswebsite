d8e20f1ad38c03ab4b5948793ceb45b9c6a66360
saurabhaws <aga.agarwal@gmail.com>
2019-08-06 08:07:44 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_jFmOWIl8T', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5fkn2ajfr52v70m5lkm61ijh03', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
